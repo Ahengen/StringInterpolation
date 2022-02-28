67afbb3141e8c9b2ad417cd6ea12cc482daf96a4
ahengen <ahengen92@gmail.com>
2022-02-27 21:10:49 -0600

    initial  commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime today = DateTime.Now;
+            Console.WriteLine($"{today,40:MMMM dd, yyyy}");
+            Console.WriteLine($"{today:yyyy.MM.dd}");
+            Console.WriteLine($"Day {today:dd} of {today:MMMM}, {today:yyyy}");
+            Console.WriteLine($"Year: {today:yyyy}, Month: {today:MM}, Day: {today:dd}");
+            Console.WriteLine($"{today,10:h:mm tt}{today,10:dddd}");
+            Console.WriteLine($"h:{today:%h}, m:{today:mm}, s:{today:ss}");
+            Console.WriteLine($"{today:yyyy.MM.dd.h.mm.ss}");
+
+            var pi = Math.PI;
+            Console.WriteLine($"{pi:c}");
+            Console.WriteLine($"{pi,10:f3}");
+        }
+    }
+}
